b3b84873f2070f6dd58ff1574c8d28fb945a779d
madhanvishanth11-cpu <madhanvishanth11@gmail.com>
2026-08-14 23:35:36 +0530

    chore: remove ResultsSection

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import CustomCursor from './components/CustomCursor';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import WhyChooseZence from './components/Stats';
-import ResultsSection from './components/ResultsSection';
 import Services from './components/Services';
 import Packages from './components/Packages';
 import WhyChooseUs from './components/WhyChooseUs';
@@ -72,7 +71,6 @@ export default function App() {
             <Hero />
             <WhyChooseZence />
             <InfiniteMarquee />
-            <ResultsSection />
             <Services />
             <Packages />
             <WhyChooseUs />
